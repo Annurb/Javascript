46cd00775f7a49ff067016cdf0c2a271d689ca8f
Brunna Brito <brunna.raphaela1@gmail.com>
2025-12-29 08:49:15 -0300

    a

---
+++ lucky7-game/src/App.jsx
@@ -0,0 +1,13 @@
+import Lucky7 from "./Lucky7";
+import Die from './Die';
+
+function App() {
+  return (
+    <>
+      <Die val={2}/>
+      <Die val={3}/>
+    </>
+  );
+}
+
+export default App;
